5e73b1ab100152c85a7fa52d9196ac80354f0163
antcin <ciniglioantonio@gmail.com>
2018-03-28 23:12:28 +0100

    Connect to mongodb, add and show rooms on app - require testing

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 var createError = require('http-errors');
+var bodyParser = require('body-parser');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
+var multer = require('multer');
+var upload = multer();
 var logger = require('morgan');
+var session = require('express-session');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -14,10 +18,13 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(upload.array());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,42 @@
 var express = require('express');
 var router = express.Router();
 
+var mongoose = require('mongoose');
+var Room = require('../models/room');
+
+mongoose.connect('mongodb://localhost/aaairbnb');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index');
+});
+
+
+
+//show rooms on rooms ejs
+router.get('/addroom', function (req, res) {
+    res.render('addroom');
+});
+
+//add rooms
+router.post('/addroom', function (req, res) {
+  // get data from view and add it to mongo db
+  Room(req.body).save(function(err,data){
+    console.log(data);
+    if (err) throw err;
+  })
+  res.redirect('/rooms');
+});
+
+
+
+//show all rooms
+router.get('/rooms', function (req, res) {
+  Room.find({}, function(err, propAll){
+    console.log(propAll);
+    if (err) throw err;
+  res.render('rooms', {'propAll': propAll});
+  });
 });
 
 module.exports = router;
