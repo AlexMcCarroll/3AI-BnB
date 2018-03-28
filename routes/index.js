6ea213b0d116ab9153a1926641ff2bddd0822136
durranee <durranee@hotmail.com>
2018-03-29 00:11:40 +0100

    properties can be seen on index page

--- routes/index.js
+++ routes/index.js
@@ -8,35 +8,14 @@ mongoose.connect('mongodb://localhost/aaairbnb');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index');
+  Room.find({}, function(err, propAll) {
+    console.log(propAll);
+    if (err) throw err;
+    res.render('index', {
+      'propAll': propAll
+    });
+  });
+
 });
-
-
-//
-// //show rooms on rooms ejs
-// router.get('/addroom', function (req, res) {
-//     res.render('addroom');
-// });
-//
-// //add rooms
-// router.post('/addroom', function (req, res) {
-//   // get data from view and add it to mongo db
-//   Room(req.body).save(function(err,data){
-//     console.log(data);
-//     if (err) throw err;
-//   })
-//   res.redirect('/rooms');
-// });
-//
-//
-//
-// //show all rooms
-// router.get('/rooms', function (req, res) {
-//   Room.find({}, function(err, propAll){
-//     console.log(propAll);
-//     if (err) throw err;
-//   res.render('rooms', {'propAll': propAll});
-//   });
-// });
 
 module.exports = router;
