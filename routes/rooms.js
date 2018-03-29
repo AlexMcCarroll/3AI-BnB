16327d0e781c7fceda940adf1497bb7335e7095a
durranee <durranee@hotmail.com>
2018-03-29 11:22:36 +0100

    fix

---
+++ routes/rooms.js
@@ -0,0 +1,35 @@
+var express = require('express');
+var router = express.Router();
+
+var mongoose = require('mongoose');
+var Room = require('../models/room');
+mongoose.connect('mongodb://localhost/aaairbnb');
+
+// /* GET home page. */
+//show all rooms
+router.get('/', function(req, res) {
+  Room.find({}, function(err, propAll) {
+    console.log(propAll);
+    if (err) throw err;
+    res.render('rooms', {
+      'propAll': propAll
+    });
+  });
+});
+
+//Goes to add room form
+router.get('/add', function(req, res) {
+  res.render('addroom');
+});
+
+// Posts add rooms form and goes back to get /room
+router.post('/add', function(req, res) {
+  // get data from view and add it to mongo db
+  Room(req.body).save(function(err, data) {
+    console.log(data);
+    if (err) throw err;
+  })
+  res.redirect('/rooms');
+});
+
+module.exports = router;
--- routes/testroutes.js
+++ routes/testroutes.js
@@ -2,7 +2,6 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-  console.log('in testroutes')
   res.render('testroutes', { hi: 'say hi to people', sum: (1+1)});
 });
 
